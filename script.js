54b445a6e2b3ae922e8f5a930b67f9b4a4f0a825
Tony <tony.kiplagat@student.moringaschool.com>
2023-07-06 12:07:50 +0300

    Finale

--- script.js
+++ script.js
@@ -5,28 +5,31 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((response) => response.json())
       .then((data) => {
         renderHome(data);
+
         movieList(data);
-        ticketDeduction(data);
       });
   }
+  // function to reneder initial homepage
   function renderHome(data) {
     data.filter((film) => {
       let random = (Math.floor(Math.random() * 15) + 1).toString();
       if (film.id === random) {
         let homeCard = document.createElement("div");
         homeCard.classList = "home_card";
+        homeCard.innerHTML = "";
         homeCard.innerHTML = `
-            <img class = "image" src= "${film.poster}" >
-            <div><h2>${film.title}</h2>
-            <p><strong>RUNTIME:</strong>${film.runtime}</p>
-            <p><strong>SHOWTIME:</strong>${film.showtime}</p>
-            <p><strong>AVAILABLE TICKETS:</strong><span id='count'>${
-              film.capacity - film.tickets_sold
-            }</span></p></div>
-            <button class = "btn">Buy Ticket</button>
-        `;
+              <img class = "image" src= "${film.poster}" >
+              <div id ='content' ><h2>${film.title}</h2>
+              <p><strong>RUNTIME:</strong>${film.runtime}</p>
+              <p><strong>SHOWTIME:</strong>${film.showtime}</p>
+              <p><strong>AVAILABLE TICKETS:</strong><span id='count'>${
+                film.capacity - film.tickets_sold
+              }</span></p></div>
+              <button id="buy_tkt" class = "btn">Buy Ticket</button>
+          `;
         let movieCard = document.getElementById("movie_card");
         movieCard.appendChild(homeCard);
+        ticketDeduction(data);
         return movieCard;
       }
     });
@@ -36,21 +39,39 @@ document.addEventListener("DOMContentLoaded", () => {
     data.forEach((film) => {
       let li = document.createElement("li");
       li.textContent = film.title;
+      li.addEventListener("click", (e) => {
+        let clickedTitle = e.target.textContent;
+        let homeCard = document.querySelector(".home_card");
+        if (film.title == clickedTitle) {
+          homeCard.innerHTML = "";
+          homeCard.innerHTML = `
+          <img class = "image" src= "${film.poster}" >
+          <div id = 'content'><h2>${film.title}</h2>
+          <p><strong>RUNTIME:</strong>${film.runtime}</p>
+          <p><strong>SHOWTIME:</strong>${film.showtime}</p>
+          <p><strong>AVAILABLE TICKETS:</strong><span id='count'>${
+            film.capacity - film.tickets_sold
+          }</span></p></div>
+          <button id="buy_tkt" class = "btn">Buy Ticket</button>
+      `;
+          let movieCard = document.getElementById("movie_card");
+          movieCard.appendChild(homeCard);
+        }
+      });
       list.appendChild(li);
     });
   }
   //add event listener to buy tickets
   function ticketDeduction(data) {
     data.forEach((film) => {
-      let ticketButton = document.querySelector(".btn");
-      let availableTicket = document.getElementById("count");
+      let homesCard = document.querySelector(".home_card");
+      console.log(homesCard);
+      let ticketButton = homesCard.querySelector(".btn");
+      let availableTicket1 = homesCard.querySelector("#content");
+      let realCount = availableTicket1.querySelector("#count");
+
       ticketButton.addEventListener("click", () => {
-        if (availableTicket > 0) {
-          availableTicket.innerHTML = film.capacity - film.tickets_sold;
-          availableTicket -= 1;
-        } else if (availableTicket == 0) {
-          availableTicket.innerHTML = "Sold out";
-        }
+        realCount.textContent = film.capacity - film.tickets_sold - 1;
       });
     });
   }
